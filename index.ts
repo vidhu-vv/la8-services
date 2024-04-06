d92e23be945aaf27c1a6b6c5574420602c03dce5
Vidhu V <79124406+vidhu-vv@users.noreply.github.com>
2024-04-06 15:30:11 -0700

    uwu

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ export const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY
 }); 
 
-const pb = new PocketBase('https://la8.continuityhost.com');
+const pb = new PocketBase('https://la8api.continuitylabs.dev');
 await pb.admins.authWithPassword(
     process.env.PB_TYPEGEN_EMAIL as string,
     process.env.PB_TYPEGEN_PASSWORD as string,
@@ -18,12 +18,11 @@ const users = await pb.collection('users').getFullList({
 });
 const languages = new Set<string>();
 for (const user of users) {
-    if (user.languages) {
-        for (const language of user.languages) {
-            languages.add(language);
-        }
+    if (user.language) {
+        languages.add(user.language)
     }
 }
+console.log(languages)
 
 
 
@@ -32,6 +31,7 @@ pb.collection('tasks').subscribe('*', async (e:any) => {
         console.log(e.action,e.record)
         const translatedLanguages: {[key: string]: string} = {};
         for(const language of languages){
+            console.log(`Translating to ${language}...`)
             const translation = await translate(e.record.title, language)
             if (translation !== null) {
                 translatedLanguages[language]=translation;
@@ -41,7 +41,6 @@ pb.collection('tasks').subscribe('*', async (e:any) => {
         for(const [language, translation] of Object.entries(translatedLanguages)){
             translationsL[language] = translation;
         }
-        console.log(translationsL)
         const record = await pb.collection('tasks').update(e.record.id, {translations: translationsL})
     }
 });
@@ -51,7 +50,7 @@ async function translate(input: string, language: string){
         model: 'gpt-3.5-turbo-1106',
         messages: [{
             role: 'system',
-            content: `You are a translator for many different languages. You have been asked to translate the following text into ${language}. Respond with only the translation.`,
+            content: `You are a translator for many different languages. You have been asked to translate the following text into ${language}. Respond with only the translation (without quotation marks). The use of quotation marks will result in a rejection of your response. If the translation language is the same as the language the input is written in, please respond with the original text.`,
         },
         {
             role: 'user',
